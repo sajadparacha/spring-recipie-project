5b2d1e925a474124b3f28a582ec8aa8c7327df51
sajadparacha <sajadparacha@gmail.com>
2020-02-28 08:47:33 +0300

    using 3 different property files for settings in case of 3 diffrernt profiles.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
-insert into Category (description) values('American');
-insert into Category (description)  values('Italian');
-insert into Category (description)  values('Pakistani');
-insert into Category (description)  values('Indian');
+insert into category (description) values('American');
+insert into category (description)  values('Italian');
+insert into category (description)  values('Pakistani');
+insert into category (description)  values('Indian');
 insert into unit_of_measure (description)  values('Cup');
 insert into unit_of_measure (description)  values('Teaspoon');
 insert into unit_of_measure (description)  values('UOM 3');
